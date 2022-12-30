ab655f0a01719a57cad76bc1812b7ba753f679c1
arda-kurama <ardakurama@gmail.com>
2022-12-30 14:24:42 -0500

    setup gif functionality with photoswipe

--- photoswipe.js
+++ photoswipe.js
@@ -3,6 +3,10 @@ const options = {
   gallery: "#my-gallery",
   children: "a",
   pswpModule: () => import("/dist/photoswipe.esm.js"),
+
+  initialZoomLevel: "fit",
+  secondaryZoomLevel: 1.5,
+  maxZoomLevel: 1,
 };
 const lightbox = new PhotoSwipeLightbox(options);
 lightbox.on("uiRegister", function () {
@@ -31,6 +35,34 @@ lightbox.on("uiRegister", function () {
           }
         }
         el.innerHTML = captionHTML || "";
+      });
+    },
+  });
+  lightbox.pswp.ui.registerElement({
+    name: "bulletsIndicator",
+    className: "pswp__bullets-indicator",
+    appendTo: "wrapper",
+    onInit: (el, pswp) => {
+      const bullets = [];
+      let bullet;
+      let prevIndex = -1;
+
+      for (let i = 0; i < pswp.getNumItems(); i++) {
+        bullet = document.createElement("div");
+        bullet.className = "pswp__bullet";
+        bullet.onclick = (e) => {
+          pswp.goTo(bullets.indexOf(e.target));
+        };
+        el.appendChild(bullet);
+        bullets.push(bullet);
+      }
+
+      pswp.on("change", (a) => {
+        if (prevIndex >= 0) {
+          bullets[prevIndex].classList.remove("pswp__bullet--active");
+        }
+        bullets[pswp.currIndex].classList.add("pswp__bullet--active");
+        prevIndex = pswp.currIndex;
       });
     },
   });
